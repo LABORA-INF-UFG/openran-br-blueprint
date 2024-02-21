93f141e0b6b8c78939fa289c2c6038f478a3dbd5
DanteCampos <dcamposfsa@gmail.com>
2024-02-21 03:00:33 +0000

    Update check appmgr registration bash

--- blueprint_v1/bouncer-xapp/check_registration.sh
+++ blueprint_v1/bouncer-xapp/check_registration.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#echo "----------------- Checking the AppMgr Health -----------------"
-#curl -H "Content-Type: application/json" -X GET http://$(kubectl get pods -n ricplt -o wide | grep appmgr | awk '{print $6}'):8080/ric/v1/health
-#echo ""
+echo "----------------- Checking if AppMgr is alive -----------------"
+curl -X GET http://$(kubectl get pods -n ricplt -o wide | grep appmgr | awk '{print $6}'):8080/ric/v1/health/alive
+echo ""
+echo "----------------- Checking if AppMgr is ready -----------------"
+curl -X GET http://$(kubectl get pods -n ricplt -o wide | grep appmgr | awk '{print $6}'):8080/ric/v1/health/ready
+echo ""
 echo "----------------- Getting AppMgr Registered xApps -----------------"
-curl -H "Content-Type: application/json" -X GET http://$(kubectl get pods -n ricplt -o wide | grep appmgr | awk '{print $6}'):8080/ric/v1/xapps
+curl -X GET http://$(kubectl get pods -n ricplt -o wide | grep appmgr | awk '{print $6}'):8080/ric/v1/xapps
